68287b6c36ade54a64257ba6095316a372024437
Yaxi Zeng <yaxizeng@usc.edu>
2024-03-15 11:25:54 +0800

    solve musics and answers
    compatible http json with Ferier Platform

--- tomato.py
+++ tomato.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, jsonify
+# -*- coding: utf-8 -*-
+import datetime
+from flask import Flask, request, jsonify, make_response
 from celery import Celery
 from task import play_music
+import time
+
 
 app = Flask(__name__)
 # app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
@@ -36,15 +40,96 @@ start_time = None
 
 @app.route('/api', methods=['POST'])
 def my_api():
+    global start_time 
     data = request.json
-    if data.get('bizname') == '狗狗去哪' and data.get('focus') == 'Puppy':
+    seq = data.get('sequence')
+    if data.get('bizname') and data.get('focus') == '番茄钟':
+        response_content = {
+            "versionid": "1.0",
+            "is_end": False,
+            "sequence": seq,
+            "timestamp": int(time.time() * 1000),
+            "directive": {
+                "directive_items": [
+                    {
+                        "content": "Welcome to the Pomodoro Technique. You can start a 25-minute focus session by saying 'Start the timer'.",
+                        "type": "1"
+                    }
+                ]
+            }
+        }
+    elif data.get('start'):
+        if start_time is None:  # 仅在start_time未初始化时设置它
+            start_time = time.time()
         # Start playing music asynchronously for 25 minutes
         play_music('Study and Relax.mp3') 
         # play_music_async.delay('Study and Relax.mp3')  # Adjust the path as necessary
-        response = {"message": "Music playing for 25 minutes"}
-    else:
-        response = {"message": "Music not playing"}
-    return jsonify(response)
+        response_content = {
+            "versionid": "1.0",
+            "is_end": False,
+            "sequence": seq,
+            "timestamp": int(time.time() * 1000),
+            "directive": {
+                "directive_items": [
+                    {
+                        "content": "You have focued for 25 minutes. Do you want to take a 5 minutes break?",
+                        "type": "1"
+                    }
+                ]
+            }
+        }
+    elif data.get('focuses') and data.get('focus') == '番茄钟':
+        if start_time is not None:
+            time_lapse = time.time() - start_time
+            time_lapse = time_lapse // 60
+            response_content = {
+                "versionid": "1.0",
+                "is_end": False,  # 根据逻辑调整是否结束会话
+                "sequence": seq,
+                "timestamp": int(time.time() * 1000),
+                "directive": {
+                    "directive_items": [
+                        {
+                            "content": "You have focused for {} minutes.".format(int(time_lapse)),
+                            "type": "1"
+                        }
+                    ]
+                }
+            }
+        else:
+            # 这里处理 start_time 为 None 的情况，比如返回一个错误消息
+            response_content = {
+                "versionid": "1.0",
+                "is_end": False,  # 根据逻辑调整是否结束会话
+                "sequence": seq,
+                "timestamp": int(time.time() * 1000),
+                "directive": {
+                    "directive_items": [
+                        {
+                            "content": "Timer has not started. Please start the timer first",
+                            "type": "1"
+                        }
+                    ]
+                }
+            }
+    elif data.get('stop') and data.get('focus') == '番茄钟':
+            response_content = {
+                "versionid": "1.0",
+                "is_end": True,  # 根据逻辑调整是否结束会话
+                "sequence": seq,
+                "timestamp": int(time.time() * 1000),
+                "directive": {
+                    "directive_items": [
+                        {
+                            "content": "I'll quit",
+                            "type": "1"
+                        }
+                    ]
+                }
+            }
+    response = make_response(jsonify(response_content))
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
+    return response
 
 
 if __name__ == '__main__':
