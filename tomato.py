93a0c3a08d94f55cc51b94d0d883d7447c7f5b48
Yaxi Zeng <yaxizeng@usc.edu>
2024-03-18 11:53:13 +0800

    refactor结束

--- tomato.py
+++ tomato.py
@@ -12,42 +12,16 @@ app = Flask(__name__)
 
 start_time = None
 
+def current_time_lapse():
+    time_lapse = time.time() - start_time
+    time_lapse = time_lapse // 60
+    return time_lapse
+
 @app.route('/api', methods=['POST'])
 def my_api():
     global start_time 
     data = request.json
     seq = data.get('sequence')
-
-def current_time():
-    time_lapse = time.time() - start_time
-    time_lapse = time_lapse // 60
-    return time_lapse
-
-    # response_content = {
-    #     "versionid": "1.0",
-    #     "is_end": False,
-    #     "sequence": seq,
-    #     "timestamp": int(time.time() * 1000),
-    #     "directive": {
-    #         "directive_items": [
-    #             {
-    #                 "content": "欢迎来到番茄钟，您可以说开启计时",
-    #                 "type": "1"
-    #             }
-    #         ]
-    #     }
-    # }
-
-    # def update_res_content(s):
-    #     response_content['directive'] = {
-    #     "directive_items": [
-    #         {
-    #             "content": s,
-    #             "type": "1"
-    #         }
-    #     ]
-    # }
-    #     return jsonify(response_content)
         
 
     # 开启番茄钟
@@ -66,71 +40,32 @@ def current_time():
     # 询问专注时间
     elif data.get('focuses') and data.get('focus') == '番茄钟':
         if start_time is not None:
-            time_lapse = time.time() - start_time
-            time_lapse = time_lapse // 60
-            formatted_content = "您已经专注了{}分钟".format(int(time_lapse))
+            formatted_content = "您已经专注了{}分钟".format(int(current_time_lapse()))
             update_res_content(formatted_content, seq)
             #还没开启倒计时
         else:
             # 这里处理 start_time 为 None 的情况，比如返回一个错误消息
             update_res_content("还没有开启倒计时，您可以说开启倒计时来记录专注时间", seq)
-            #放弃专注
+
+    #放弃专注
     elif data.get('stop') and data.get('break') is None:
         if start_time is not None:
-            time_lapse = time.time() - start_time
-            time_lapse = time_lapse // 60
-            s = "您已经专注了{}分钟,那我就先退出了".format(int(time_lapse))
+            s = "您已经专注了{}分钟,那我就先退出了".format(int(current_time_lapse()))
         else:
             s = "没什么事我就先退出了"
         stop_music()
-        response_content = {
-            "versionid": "1.0",
-            "is_end": True,  # 根据逻辑调整是否结束会话
-            "sequence": seq,
-            "timestamp": int(time.time() * 1000),
-            "directive": {
-                "directive_items": [
-                    {
-                        "content": s,
-                        "type": "1"
-                    }
-                ]
-            }
-        }
+        update_res_content(s, seq)
+
     #放弃休息
     elif data.get('stop') and data.get('break'):
         stop_music()
-        response_content = {
-            "versionid": "1.0",
-            "is_end": False,
-            "sequence": seq,
-            "timestamp": int(time.time() * 1000),
-            "directive": {
-                "directive_items": [
-                    {
-                        "content": "休息结束，您可以说“开启计时”或者放弃计时",
-                        "type": "1"
-                    }
-                ]
-            }
-        }
+        update_res_content("休息结束，您可以说“开启计时”或者放弃计时", seq)
         #开启休息
     elif data.get('start') and data.get('break'):
         play_break('Study and Relax.mp3')
-        response_content = {
-            "versionid": "1.0",
-            "is_end": False,
-            "sequence": seq,
-            "timestamp": int(time.time() * 1000),
-            "directive": {
-                "directive_items": [
-                    {
-                        "content": "休息结束，您可以说“开启计时”或者放弃计时",
-                        "type": "1"
-                    }
-                ]
-            }
-        }
+        update_res_content("休息结束，您可以说“开启计时”或者放弃计时", seq)
+
+
     # 未识别的意图
     else:
             response_content = {
